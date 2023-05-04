a851747674b535101f948f96dd8b2cc9d3e7f87d
megatron0000 <vitor_pimenta97@hotmail.com>
2023-05-03 20:53:11 -0400

    style: use double quotes in webpack config file

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,47 +1,46 @@
-const path = require('path');
+const path = require("path");
 const webpack = require("webpack");
 
-const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
-
+const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: "./src/index.ts",
   mode: "development",
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
+        use: "ts-loader",
+        exclude: /node_modules/
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ["style-loader", "css-loader"]
       },
       // fix: codicons in Monaco dot not appear.
       // In webpack 5, assets are no longer loaded with "file-loader".
       // https://stackoverflow.com/questions/71674567/monaco-editor-doesnt-load-codicons-in-case-of-using-webpack
       {
         test: /\.ttf$/,
-        type: 'asset/resource'
+        type: "asset/resource"
       }
     ]
   },
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: [".ts", ".js"]
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build")
   },
   plugins: [
     // https://www.npmjs.com/package/monaco-editor-webpack-plugin
     new MonacoWebpackPlugin()
   ],
   cache: {
-    type: 'filesystem'
+    type: "filesystem"
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   optimization: {
     // Avoid minimization because TerserPlugin (uglifier) is slow
     // https://github.com/webpack-contrib/terser-webpack-plugin/issues/217
