aecac5a33b310b989909e32de505312863e7ace3
megatron0000 <vitor_pimenta97@hotmail.com>
2023-07-29 19:25:44 -0400

    feat: implement "runAll" in the simulator

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -50,6 +50,9 @@ const buttonPrevMacro = document.getElementById(
 const buttonNextMacro = document.getElementById(
   "button-next-macro"
 ) as HTMLButtonElement;
+const buttonRunAll = document.getElementById(
+  "button-run-all"
+) as HTMLButtonElement;
 const buttonEdit = document.getElementById("button-edit") as HTMLButtonElement;
 
 let controller = new VisualizationController(
@@ -99,7 +102,7 @@ buttonVisualize.addEventListener("click", async () => {
     controller.reset(editor.getValue());
     controller.renderCurrentStep();
   });
-  maybeDisableStepButtons();
+  updateStepButtons();
 
   buttonVisualize.disabled = false;
   buttonVisualize.textContent = "Executar";
@@ -117,31 +120,73 @@ buttonVisualize.textContent = "Executar";
  * 4: setup logic for #visualize page
  */
 
-function maybeDisableStepButtons() {
+function updateStepButtons() {
   buttonPrevMicro.disabled = controller.isFirstStep();
   buttonNextMicro.disabled = controller.isLastStep();
   buttonPrevMacro.disabled = controller.isFirstStep();
   buttonNextMacro.disabled = controller.isLastStep();
 }
 
+function disableStepButtons() {
+  buttonPrevMicro.disabled = true;
+  buttonNextMicro.disabled = true;
+  buttonPrevMacro.disabled = true;
+  buttonNextMacro.disabled = true;
+}
+
+function disableEditButton() {
+  buttonEdit.disabled = true;
+}
+
+function enableEditButton() {
+  buttonEdit.disabled = false;
+}
+
 buttonPrevMicro.addEventListener("click", () => {
   ifErrorOpenModal(() => controller.backwardStep("micro"));
-  maybeDisableStepButtons();
+  updateStepButtons();
 });
 
 buttonNextMicro.addEventListener("click", () => {
   ifErrorOpenModal(() => controller.advanceStep("micro"));
-  maybeDisableStepButtons();
+  updateStepButtons();
 });
 
 buttonPrevMacro.addEventListener("click", () => {
   ifErrorOpenModal(() => controller.backwardStep("macro"));
-  maybeDisableStepButtons();
+  updateStepButtons();
 });
 
 buttonNextMacro.addEventListener("click", () => {
   ifErrorOpenModal(() => controller.advanceStep("macro"));
-  maybeDisableStepButtons();
+  updateStepButtons();
+});
+
+const ICON_ARROWS_DOWN_TO_LINE =
+  '<i class="fa-solid fa-arrows-down-to-line"></i>';
+
+const ICON_PAUSE = '<i class="fa fa-pause"></i>';
+
+let isRunningAll = false;
+
+buttonRunAll.addEventListener("click", async () => {
+  if (isRunningAll) {
+    isRunningAll = false; // stop the loop below
+    return;
+  }
+
+  disableStepButtons();
+  disableEditButton();
+  buttonRunAll.innerHTML = ICON_PAUSE;
+  isRunningAll = true;
+  while (isRunningAll && !controller.isLastStep()) {
+    if (ifErrorOpenModal(() => controller.advanceStep("macro"))) break;
+    await sleep(10); // async so that the UI does not freeze (the user can pause)
+  }
+  isRunningAll = false;
+  buttonRunAll.innerHTML = ICON_ARROWS_DOWN_TO_LINE;
+  enableEditButton();
+  updateStepButtons();
 });
 
 buttonEdit.addEventListener("click", () => {
@@ -149,11 +194,20 @@ buttonEdit.addEventListener("click", () => {
   buttonVisualize.style.display = "block";
 });
 
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+/**
+ * @returns false if the callback did not throw. true if it threw an error
+ */
 function ifErrorOpenModal(callback: Function) {
   try {
     callback();
+    return false;
   } catch (err) {
     showErrorModal(editor.getValue(), err);
+    return true;
   }
 }
 
